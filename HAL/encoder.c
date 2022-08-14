2f2b723cae903fde9e07cc037bceff0cdc0e099f
Ahmed Maged <81818994+AhmedMagedAlsharif@users.noreply.github.com>
2022-08-14 19:58:55 +0200

    Add files via upload

--- HAL/encoder.c
+++ HAL/encoder.c
@@ -1,17 +1,18 @@
 #include "encoder.h"
 
-int16_t Encoder_Value;
+static int16_t Encoder_Value;
 
 void Encoder_Init(void)
 {
-    GPIO_Init(GPIOB, GPIO_PIN2|GPIO_PIN3, GPIO_Mode_IN_PU);
+    GPIO_Init(ENCODER_PORT, ENCODER_CHA_PIN|ENCODER_CHB_PIN, GPIO_Mode_IN_PU);
     Encoder_Value = 0;
 }
+
 void Encoder_Update(void)
 {
     uint8_t Cur_State = 0;
     static uint8_t Prev_State = 255;
-    Cur_State = ((!(PINB&0x01))|((!(PINB&0x02))<<1));
+    Cur_State = ((!(ENCODER_PIN&ENCODER_CHA_PIN))|((!(ENCODER_PIN&ENCODER_CHB_PIN))<<1));
     switch(Cur_State)
     {
         case 0x00:
--- HAL/encoder.h
+++ HAL/encoder.h
@@ -8,20 +8,17 @@
 #ifndef ENCODER_H
 #define	ENCODER_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
 #include "gpio.h"
+
+    /*Port and Pins config*/
+#define ENCODER_PORT            GPIOB
+#define ENCODER_PIN             PINB
+#define ENCODER_CHA_PIN         GPIO_PIN0
+#define ENCODER_CHB_PIN         GPIO_PIN1
     
 void Encoder_Init(void);
 void Encoder_Update(void);
 int16_t Encoder_Get_Value(void);
-
-
-#ifdef	__cplusplus
-}
-#endif
 
 #endif	/* ENCODER_H */
 
